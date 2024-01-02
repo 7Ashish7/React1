d0b36a5d925b7110009977f4259ef0f594f7fb63
Ashish Kumar <ashish.rmd2@gmail.com>
2024-01-02 21:03:58 +0530

    train

--- App.js
+++ App.js
@@ -22,13 +22,11 @@ const Header=()=>{
 const Rescard=()=>{
     return (
         <div className="rescard">
-            <div className="resimg">
                 <img className="foodimg" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/90e65f9bfc54de0d1da6a000def0ea2b" alt="foodimg" />
-            </div>
-        <div className="name">Darbar Veg Restaurant</div>
-        <div className="cusines">Chinese North Indian</div>
-        <div className="stars">4.4 stars</div>
-        <div className="price">240 rupees</div>
+        <h3>Darbar Veg Restaurant</h3>
+        <h4>Chinese North Indian</h4>
+        <h4>4.4 stars</h4>
+        <h4>240 rupees</h4>
         </div>
         
     )
